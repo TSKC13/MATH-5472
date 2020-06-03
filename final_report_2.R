284ae2b341b1541b55becb2b3f81fdf6d229421b
TSKC13 <35163116+TSKC13@users.noreply.github.com>
2020-06-03 23:51:04 +0800

    Add files via upload

---
+++ final_report_2.R
@@ -0,0 +1,38 @@
+load("E:/OneDrive - HKUST Connect/港科课程/第二学期/MATH5472 Computer Age Statistical Inference with Applications/final report/chao.RData")
+library(varbvs)
+library(glmnet)
+load("E:/OneDrive - HKUST Connect/港科课程/第二学期/MATH5472 Computer Age Statistical Inference with Applications/final report/PCA.RData") # covaraites from python's PCA
+
+vbvs = varbvs(X=newX,y=newY,family='gaussian',Z=new_Z)
+
+rmse = apply(vbvs$residuals ^ 2,MARGIN=2, function(x) sqrt(mean(x)))
+
+plot(vbvs$logodds, rmse, type='b',col='black',xlab="logodds",
+     ylab="rmse")
+
+LB = vbvs$mu - 1.96 * sqrt(vbvs$s)
+UB = vbvs$mu + 1.96 * sqrt(vbvs$s)
+non_zero = (UB < 0) | (LB > 0)
+non_zero_count = colSums(non_zero)
+
+plot(vbvs$logodds, rev(non_zero_count), type='b',col='black',xlab="logodds",
+     ylab="non-zero variables")
+
+plot(vbvs$logodds, colMeans(vbvs$alpha), type='b',col='black',xlab="logodds",
+     ylab=expression(posterior ~  ~ probability ~ of ~ pi))
+
+
+
+
+
+fit.glmnet <- cv.glmnet(x=newX,y=newY, family = "gaussian", alpha = 1, 
+                        lambda = 10^(seq(0, -2, -0.05)),nfolds = 20)
+
+
+plot(fit.glmnet$lambda, fit.glmnet$cvm, xlab="lambda",ylab="cross-validation error",type='l',col='blue',ylim=c(0.95,1.3))
+lines(fit.glmnet$lambda, fit.glmnet$cvup)
+lines(fit.glmnet$lambda, fit.glmnet$cvlo)
+
+
+plot(fit.glmnet$lambda, fit.glmnet$nzero,xlab="lambda",ylab="non-zero variabls",type='l',col='blue')
+
